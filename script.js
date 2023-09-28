bdc62060f118b9cbb85af18c07b488ea3dcf171d
Arthur Ritzel <ritzelarthur@gmail.com>
2023-09-28 16:21:12 -0300

    lateral e frente ok

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Inicializa uma variável para armazenar a soma do valor dos lotes selecionados
 var soma = 0
-
+const num_lotes_cima = 14;
 // Seleciona o elemento com o id "subtotal" e armazena em uma variável
 var subtot = document.querySelector("#subtotal")
 
 // Inicializa uma variável para armazenar a soma dos metros de frente dos lotes selecionados
 var metros_frente = 0
-
+var metros_lateral = 0
+var primeira_linha = 0;
+var segunda_linha = 0;
+var tamanho_metro1 = 0;
+var tamanho_metro2 = 0;
 // Define a URL da API de onde os dados serão buscados
 const URL = "https://8b6mkftr.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22quadra7%22%5D%7B%0A++lote%2C%0A++++metros_lateral%2C%0A++++metros_frente%2C%0A++++disponivel%2C%0A++++valor%2C%0A++++%27imagem%27%3A+imagem.asset-%3E+url%0A%7D&perspective=published"
 
@@ -29,7 +33,11 @@ async function quadra7(){
             if(position+1 == element.lote){
                 // Cria uma div com a classe "l1" e o id igual ao número do lote
                 var div = document.createElement("div")
-                div.className = "l1"
+                if(element.lote <= num_lotes_cima){
+                    div.className = "l1"
+                }else{
+                    div.setAttribute("class", "l1 l2")
+                }
                 div.id = element.lote
                 
                 // Cria uma imagem e define seu atributo src com a URL da imagem
@@ -55,7 +63,7 @@ async function quadra7(){
 
     // Função para atualizar o subtotal na página
     function subtotal(){
-        subtot.innerHTML = `Subtotal: ${soma.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} <br> metros frente: ${metros_frente}`
+        subtot.innerHTML = `Subtotal: ${soma.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} <br> metros frente: ${metros_frente} <br> metros lateral ${metros_lateral}`
     }
 
     // Seleciona todos os elementos com a classe "l1" (as divs criadas anteriormente)
@@ -72,35 +80,159 @@ async function quadra7(){
         if(exist == 1){
             // Adiciona um evento de clique à div se o lote estiver disponível
             element2.addEventListener("click", ()=>{
-                if(element2.style.backgroundColor == "rgba(30, 255, 0, 0.404)"){
-                    element2.style.backgroundColor = ""
+                if(element2.style.border == "4px solid rgb(8, 110, 5)"){
+                    element2.style.border = "1px solid black"
                     for(let i = 0; i < (dadosJS.result).length; i++){
                         if(element2.id == dadosJS.result[i].lote){
                             // Subtrai o valor e os metros de frente quando desselecionado
                             soma -= parseFloat(dadosJS.result[i].valor)
-                            metros_frente -= parseFloat(dadosJS.result[i].metros_frente)
+                           
+
+                            if(element2.className == "l1"){
+                                primeira_linha--
+                            }else if(element2.className == "l1 l2"){
+                                segunda_linha--
+                            }
+                            
+                            if(element2.className == "l1" && primeira_linha == 0){
+                                metros_lateral -= tamanho_metro1
+                            }else if(element2.className == "l1 l2" && segunda_linha == 0){
+                                metros_lateral -= tamanho_metro2
+                            }
+                            if(primeira_linha == 0 && segunda_linha == 0){
+                                metros_lateral = 0;
+                            }
+
+                            if(!document.querySelector(`#a${parseInt(element2.id) + 14}[class = "select"`) && element2.id <= 14){
+                                metros_frente -= parseFloat(dadosJS.result[i].metros_frente)
+                            }else if(document.querySelector(`#a${parseInt(element2.id) + 14}[class = "select"`) && element2.style.width > (document.getElementById(`${parseInt(element2.id) + 14}`)).style.width){
+                                
+                                for(let k = 0; k < (dadosJS.result).length; k++){
+                                    
+                                    if(dadosJS.result[k].lote == (parseInt(element2.id) + 14)){
+                                       
+                                        metros_frente += dadosJS.result[k].metros_frente
+                                    }
+                                    if(dadosJS.result[k].lote == element2.id){
+                                        metros_frente -= dadosJS.result[k].metros_frente
+                                        
+                                    }
+                                }
+                            }
+                            
+                            
+                            if(!document.querySelector(`#a${parseInt(element2.id) - 14}[class = "select"`) && element2.id >= 14){
+                                metros_frente -= parseFloat(dadosJS.result[i].metros_frente)
+                            }else if(document.querySelector(`#a${parseInt(element2.id) - 14}[class = "select"`) && element2.style.width > (document.getElementById(`${parseInt(element2.id) - 14}`)).style.width){
+                                
+                                for(let k = 0; k < (dadosJS.result).length; k++){
+                                    
+                                    if(dadosJS.result[k].lote == (parseInt(element2.id) - 14)){
+                                       
+                                        metros_frente += dadosJS.result[k].metros_frente
+                                    }
+                                    if(dadosJS.result[k].lote == element2.id){
+                                        metros_frente -= dadosJS.result[k].metros_frente
+                                        
+                                    }
+                                }
+
+                               
+                            }
+                            
                             element2.style.border = "1px solid black"
                             break;
                         }
                     }
                     // Remove o elemento selecionado da lista de escolhas
-                    var remo = document.querySelector(`#select[class = '${element2.className}'`)
+                    var remo = document.querySelector(`#a${element2.id}[class = 'select`)
                     var esc = document.querySelector(".escolhas")
-                    console.log(remo)
+                    
                     esc.removeChild( remo );
                 }else{
-                    element2.style.backgroundColor = "#1eff0067"
-                    var div = element2.cloneNode(true)
-                    div.id = "select"
+                    
                     for(let i = 0; i < 100; i++){
                         if(element2.id == dadosJS.result[i].lote){
                             // Adiciona o valor e os metros de frente quando selecionado
                             soma += parseFloat(dadosJS.result[i].valor)
-                            metros_frente += parseFloat(dadosJS.result[i].metros_frente)
+
+                            
+
+                            if(element2.className == "l1" && primeira_linha == 0){
+
+                                metros_lateral += parseFloat(dadosJS.result[i].metros_lateral)
+                                tamanho_metro1 = dadosJS.result[i].metros_lateral
+
+                            }else if(element2.className == "l1 l2" && segunda_linha == 0){
+
+                                metros_lateral += parseFloat(dadosJS.result[i].metros_lateral)
+                                tamanho_metro2 = dadosJS.result[i].metros_lateral
+
+                            }
+
+                            if(element2.className == "l1"){
+                                if(dadosJS.result[i].metros_lateral > tamanho_metro1){
+                                    metros_lateral -= tamanho_metro1
+                                    metros_lateral += dadosJS.result[i].metros_lateral
+                                    tamanho_metro1 = dadosJS.result[i].metros_lateral
+                                }
+                                primeira_linha++
+                            }else if(element2.className == "l1 l2"){
+                                if(dadosJS.result[i].metros_lateral > tamanho_metro2){
+                                    metros_lateral -= tamanho_metro1
+                                    metros_lateral += dadosJS.result[i].metros_lateral
+                                    tamanho_metro2 = dadosJS.result[i].metros_lateral
+                                }
+                                segunda_linha++
+                            }
+
+                            if(!document.querySelector(`#a${parseInt(element2.id) + 14}[class = "select"`) && element2.id <= 14){
+                                metros_frente += parseFloat(dadosJS.result[i].metros_frente)
+                                
+                            }else if(document.querySelector(`#a${parseInt(element2.id) + 14}[class = "select"`) && element2.style.width > (document.getElementById(`${parseInt(element2.id) + 14}`)).style.width){
+                                for(let k = 0; k < (dadosJS.result).length; k++){
+                                    
+                                    if(dadosJS.result[k].lote == (parseInt(element2.id) + 14)){
+                                       
+                                        metros_frente -= dadosJS.result[k].metros_frente
+                                    }
+                                    if(dadosJS.result[k].lote == element2.id){
+                                        metros_frente += dadosJS.result[k].metros_frente
+                                        
+                                    }
+                                }
+                                
+                            }
+                            
+                            console.log((document.querySelector(`#a${parseInt(element2.id) + 14}[class = "select"`) && element2.style.width > (document.getElementById(`${parseInt(element2.id) + 14}`)).style.width))
+                            
+                            if(!document.querySelector(`#a${parseInt(element2.id) - 14}[class = "select"`) && element2.id >= 14){
+                                    metros_frente += parseFloat(dadosJS.result[i].metros_frente)
+                            }else if(document.querySelector(`#a${parseInt(element2.id) - 14}[class = "select"`) && element2.style.width > (document.getElementById(`${parseInt(element2.id) - 14}`)).style.width){
+                                
+                                for(let k = 0; k < (dadosJS.result).length; k++){
+                                    
+                                    if(dadosJS.result[k].lote == (parseInt(element2.id) - 14)){
+                                       
+                                        metros_frente -= dadosJS.result[k].metros_frente
+                                    }
+                                    if(dadosJS.result[k].lote == element2.id){
+                                        metros_frente += dadosJS.result[k].metros_frente
+                                        
+                                    }
+                                }
+
+                               
+                            }
+                            console.log(element2.style.width)
                             element2.style.border = "4px solid rgb(8, 110, 5)"
                             break;
                         }
                     }
+                    
+                    var div = element2.cloneNode(true)
+                    div.className = "select"
+                    div.id = `a${element2.id}`
                     // Adiciona o elemento selecionado à lista de escolhas
                     document.querySelector(".escolhas").append(div)
                 }
